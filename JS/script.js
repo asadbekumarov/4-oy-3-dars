4e92fd88a99f19793871ac70961f2e8da4aa72ff
asadbekumarov <asadbekumarov922@gmail.com>
2024-06-26 23:12:53 +0500

    dars

---
+++ JS/script.js
@@ -0,0 +1,87 @@
+
+// let score = prompt("Enter your score");
+// if (score >= 0 && score <= 80) {
+//   alert("You could not enter the study");
+// } else if (score >= 80 && score <= 100) {
+//     alert("You have been recommended to study on the basis of a super-contract. The amount of the contract is $3000 per year, how much money do you have?")
+//   var money = prompt("The amount of the contract is $3000 per year, how much money do you have?");
+//   if (money >= 3000) {
+//     alert("You have been admitted to the course");
+//   } else {
+//     alert("You do not have enough funds to enter the bank");
+//   }
+// } else if (score >= 100 && score < 130) {
+//   var pul = 2000;
+//   var message = `You have been recommended to study on the basis of a super-contract. The amount of the contract is $2000 per year, how much money do you have?`;
+//   var money = prompt("The amount of the contract is $2000 per year, how much money do you have?");
+//   if (money >= 2000) {
+//     alert("You have been admitted to the course");
+//   } else {
+//     alert("You do not have enough funds to enter the bank");
+//   }
+// } else if (score >= 130 && score < 180) {
+//     alert("You have been accepted to study on the basis of a grant")
+//     } else {
+//       alert("Error value");
+//     }
+
+/*2-masala*/
+// let number = prompt("Kiritgan soningizni jub yoki toq ekanligini aniqlab beraman")
+// if (number % 2 === 0 ) {
+// alert("Siz kiritgan son jub son");
+// console.log(true);
+// }else{
+//   alert("Siz kiritgan son toq son")
+// console.log(false);
+// }
+
+/*3-masala*/
+
+
+// let a = prompt("Kiritgan soningizni musbat yoki manfiy ekanligini aniqlab beraman")
+// if( a > 0 ) {
+//     alert("kiritgan soningiz musabt son")
+//     console.log(true);
+// } else{
+//     alert("kiritgan soningiz manfiy son")
+//     console.log(false);
+// }
+/*4-masala*/
+
+// let a = prompt("son kiriting")
+// let b =prompt ("yana son kiriting")
+// if(a > 2 & b <= 3) {
+//     alert("kiritgan soningiz butun son")
+//         console.log(true);
+// }else{
+//     alert("kiritgan soningiz butun son emas")
+//         console.log(false);
+//   }
+
+/*5-masala*/
+//  let number1 = prompt("birinchi sonni kiriting")
+//  let number2 = prompt(" ikkinchi sonni kiriting")
+//  let number3 = prompt("oxirgi sonningizni kirting")
+// if(number1 <= number2 && number2 <= number3 ) {
+//   alert("Kiritgan sonlaringiz butun son ekan.")
+//   console.log(true)
+// }else{
+//   alert("Kiritgan sonlaringiz butun son emas.")
+//   console.log(false)
+// }
+ /*6-masala*/
+
+//  let number1 =prompt ("birinci son")
+//  let number2 =prompt ("ikkinchi sonni kiriting")
+//  if (number1 % 2 & number2 % 2 & number1 % 3 & number2 % 3) {
+//   alert("juft")
+//  } else{
+//   alert("toq")
+//  }
+
+
+
+
+ 
+  
+
